623896ac4cd1f28042e43edc65481a0418f46d5d
Roman Yacenko <ryacenko3@gmail.com>
2021-10-06 13:57:04 +0300

    solution

--- listOfCuisenes.js
+++ listOfCuisenes.js
@@ -9,9 +9,13 @@ const cuisines = [
   {name: 'Chinese', recipes: 145, image: './images/cuisines/Chinese.png'},
   {name: 'Pizza', recipes: 327, image: './images/cuisines/Pizza.png'},
   {name: 'American', recipes: 327, image: './images/cuisines/America.png'},
-]
+];
+
+const secondCuisines = [...cuisines];
 
 const cuisinesBlock = document.querySelector('.cuisines__container');
+let sorted = false;
+const SortButton = document.getElementById('sort');
 
 cuisines.map(cuisine => {
   cuisinesBlock.innerHTML += `
@@ -24,4 +28,38 @@ cuisines.map(cuisine => {
       <span class="cuisines__name-of-cuisine">${cuisine.name}</span>
     </div>
   `
+})
+
+SortButton.addEventListener('click', () => {
+  sorted = !sorted;
+  cuisinesBlock.innerHTML = '';
+  console.log(123)
+
+  if (sorted) {
+    cuisines.sort((a, b) => a.recipes - b.recipes).map(cuisine => {
+      cuisinesBlock.innerHTML += `
+        <div class="cuisines__cuisine" style="
+          background: url('${cuisine.image}');
+          background-size: cover;
+          background-repeat: no-repeat;
+        ">
+          <span class="cuisines__amount-of-recipes">${cuisine.recipes} Recipes</span>
+          <span class="cuisines__name-of-cuisine">${cuisine.name}</span>
+        </div>
+      `
+    })
+  } else {
+    secondCuisines.map(cuisine => {
+      cuisinesBlock.innerHTML += `
+        <div class="cuisines__cuisine" style="
+          background: url('${cuisine.image}');
+          background-size: cover;
+          background-repeat: no-repeat;
+        ">
+          <span class="cuisines__amount-of-recipes">${cuisine.recipes} Recipes</span>
+          <span class="cuisines__name-of-cuisine">${cuisine.name}</span>
+        </div>
+      `
+    })
+  }
 })
